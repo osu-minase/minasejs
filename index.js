748e7ac403d6aedb9bb5b86bdea67c469ea77a7b
KotypeyPyEdition <mr.unionchik@gmail.com>
2019-10-27 01:55:35 +0300

    latest commmit for today, added some pages and another...

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 1337;
-const host = 'localhost';
+const port = 3000;
+const host = '127.0.0.1';
+app.set('view engine', 'ejs');
+//IMPORTS
+app.use(express.static('static'));
+const homepage = require('./routes/homepage');
 
-app.listen(3000, 'localhost', () => {
+app.use('/', homepage)
+app.listen(3000, host, () => {
     console.log(`Listening on http://${host}:${port}`)
 })
--- routes/homepage.js
+++ routes/homepage.js
@@ -1,1 +1,8 @@
 //just a test
+const router = require('express').Router();
+router.get('/', (req, res) => {
+    res.render('./pages/homepage');
+});
+
+
+module.exports = router;
