966be08913d3ced5c3f23c41cadac1432de3cab0
KotypeyPyEdition <mr.unionchik@gmail.com>
2019-11-17 01:36:54 +0200

    tests x3

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,6 +5,6 @@ const joi = require('@hapi/joi');//Validation
 
 //I go sleep, tomorrow will continiune
 route.get('/login', async (req, res) => {
-    res.sendFile('https://media.discordapp.net/attachments/604665666166980613/645352461338411039/1573798927951.jpg')
+    res.sendFile('../../img/401.jpg')
 })
 module.exports = route;
