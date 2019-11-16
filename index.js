36750b633eb06ec140b244473f9c4622ef71c0f4
KotypeyPyEdition <mr.unionchik@gmail.com>
2019-11-16 18:01:33 +0200

    configuration

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const config = require('./config.json'); //CONFIG
 const express = require('express');
 const mysql = require('mysql2/promise');
 const bodyParser = require('body-parser');
-const homepage_route = require('./routes/homepage')
+const homepage_route = require('./routes/homepage');
+const redis = require('redis');
+const redis = new redis.createClient({host: config.redis.host, db: config.redis.db, password: config.redis.password})
 global.app = express();
-options = {host: 'localhost',user: 'root',database: 'ripple'};
+options = {host: config.database.host,user: config.database.user, password: config.database.password, database: config.database.database};
 (async () => {
     global.db = await mysql.createPool(options);
     app.use(bodyParser.json());
@@ -20,7 +23,7 @@ options = {host: 'localhost',user: 'root',database: 'ripple'};
 
 
 
-    const PORT = process.env.PORT || 8080;
+    const PORT = config.webserver.port || 8080;
     app.listen(PORT, () => {
         console.log(`app running on port ${PORT}`);
     })
