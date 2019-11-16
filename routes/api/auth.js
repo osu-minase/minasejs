a8aa003f8c5730a4dabd7f502620adca16fe07a2
KotypeyPyEdition <mr.unionchik@gmail.com>
2019-11-17 01:31:51 +0200

    just a tests

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,5 +4,7 @@ const jwt = require('jsonwebtoken');//Tokens
 const joi = require('@hapi/joi');//Validation
 
 //I go sleep, tomorrow will continiune
-
+route.get('/login', async (req, res) => {
+    res.sendFile('https://media.discordapp.net/attachments/604665666166980613/645352461338411039/1573798927951.jpg')
+})
 module.exports = route;
