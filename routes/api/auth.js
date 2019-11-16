dde5de5a3eb4445d55b67a087395339c96998e38
KotypeyPyEdition <mr.unionchik@gmail.com>
2019-11-17 01:20:21 +0200

    Login api start...

---
+++ routes/api/auth.js
@@ -0,0 +1,8 @@
+const route = require('express').Router();//Router, not wifi /shrug
+const bcrypt = require('bcryptjs');//Password hashes
+const jwt = require('jsonwebtoken');//Tokens
+const joi = require('@hapi/joi');//Validation
+
+//I go sleep, tomorrow will continiune
+
+module.exports = route;
