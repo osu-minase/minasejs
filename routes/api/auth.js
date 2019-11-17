437029d617f7aa58306595882d41f8e1dd59f244
KotypeyPyEdition <mr.unionchik@gmail.com>
2019-11-17 12:43:47 +0200

    slish, tebe suda nelzya

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,6 +5,6 @@ const joi = require('@hapi/joi');//Validation
 
 //I go sleep, tomorrow will continiune
 route.get('/login', async (req, res) => {
-    res.sendFile('./img/401.jpg')
+    res.render('401')
 })
 module.exports = route;
